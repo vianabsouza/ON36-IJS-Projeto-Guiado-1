e98b4737a718740914e78f50c136f2f73300b8f0
Barbara Viana <vianabsouza@gmail.com>
2024-08-21 18:23:50 -0300

    feat: add student CRUD structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AlunosModule } from './alunos/alunos.module';
 
 @Module({
-  imports: [],
+  imports: [AlunosModule],
   controllers: [AppController],
   providers: [AppService],
 })
