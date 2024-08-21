37ab198603b932be9e7ce6843186ddf7c7b571f1
Barbara Viana <vianabsouza@gmail.com>
2024-08-21 19:17:16 -0300

    refactor: remove service files

--- src/alunos/alunos.controller.spec.ts
+++ src/alunos/alunos.controller.spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AlunosController } from './alunos.controller';
-import { AlunosService } from './alunos.service';
 
 describe('AlunosController', () => {
   let controller: AlunosController;
@@ -8,7 +7,7 @@ describe('AlunosController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AlunosController],
-      providers: [AlunosService],
+      providers: [],
     }).compile();
 
     controller = module.get<AlunosController>(AlunosController);
--- src/alunos/alunos.module.ts
+++ src/alunos/alunos.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AlunosService } from './alunos.service';
 import { AlunosController } from './alunos.controller';
 
 @Module({
   controllers: [AlunosController],
-  providers: [AlunosService],
+  providers: [],
 })
 export class AlunosModule {}
